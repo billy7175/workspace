3a8adc0a82e94b478d809650bd1b7e8731b3d611
billy7175 <68532934+billy7175@users.noreply.github.com>
2021-12-26 16:43:50 +0900

    Reading servlet parameters

--- servletdemo/src/main/java/com/luv2code/servletdemo/StudentServlet.java
+++ servletdemo/src/main/java/com/luv2code/servletdemo/StudentServlet.java
@@ -2,6 +2,7 @@ package com.luv2code.servletdemo;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -9,16 +10,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class HelloWorldServlet
+ * Servlet implementation class StudentServlet
  */
-@WebServlet("/HelloWorldServlet")
-public class HelloWorldServlet extends HttpServlet {
+@WebServlet("/StudentServlet")
+public class StudentServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public HelloWorldServlet() {
+    public StudentServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -27,24 +28,25 @@ public class HelloWorldServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-//		response.getWriter().append("Served at: ").append(request.getContextPath());
-		// Step 1: set the content type
-				response.setContentType("text/html");
-				
-				
-				PrintWriter out = response.getWriter();
-				
-				// Step 3: generate HTML content
-				out.println("<html><body>");
-				
-				out.println("<h2>Hello World</h2>");
-				out.println("<hr>");
-				out.println("Time on the server is: " + new java.util.Date());
-				
-				out.println("</body></html>");
+
+		// Step 1: set content type
+		response.setContentType("text/html");
+		
+		// Step 2: get the printwriter
+		PrintWriter out = response.getWriter();
+		
+		// Step 3: generate the HTML content
+		out.println("<html><body>");
+
+		out.println("The student is confirmed: "
+					+ request.getParameter("firstName") + " "
+					+ request.getParameter("lastName"));
+		
+		out.println("</body></html>");
+		
 	}
 
+	
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
--- servletdemo/src/main/java/com/luv2code/servletdemo/TestParamServlet.java
+++ servletdemo/src/main/java/com/luv2code/servletdemo/TestParamServlet.java
@@ -2,6 +2,8 @@ package com.luv2code.servletdemo;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -9,16 +11,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class HelloWorldServlet
+ * Servlet implementation class TestParamServlet
  */
-@WebServlet("/HelloWorldServlet")
-public class HelloWorldServlet extends HttpServlet {
+@WebServlet("/TestParamServlet")
+public class TestParamServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public HelloWorldServlet() {
+    public TestParamServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -27,24 +29,39 @@ public class HelloWorldServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-//		response.getWriter().append("Served at: ").append(request.getContextPath());
-		// Step 1: set the content type
-				response.setContentType("text/html");
-				
-				
-				PrintWriter out = response.getWriter();
-				
-				// Step 3: generate HTML content
-				out.println("<html><body>");
-				
-				out.println("<h2>Hello World</h2>");
-				out.println("<hr>");
-				out.println("Time on the server is: " + new java.util.Date());
-				
-				out.println("</body></html>");
+
+		// Step 1: set content type
+		response.setContentType("text/html");
+		
+		// Step 2: get printwriter
+		PrintWriter out = response.getWriter();
+		
+		// Step 3: read configuration params
+		ServletContext context = getServletContext(); // inherit from HttpServlet
+
+		String maxCartSize = context.getInitParameter("max-shopping-cart-size");
+		String teamName = context.getInitParameter("project-team-name");
+		
+		// Step 4: generate HTML content
+		out.println("<html><body>");
+
+		out.println("Max cart: " + maxCartSize);
+		out.println("<br/><br/>");
+		out.println("Team name: " + teamName);
+		
+		out.println("</body></html>");	
 	}
 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
